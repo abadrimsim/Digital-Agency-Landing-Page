76d0fa77908b0c8be3c107cb3ea5b841c3de8724
Angel <abadrimsim@gmail.com>
2022-01-05 23:31:19 +0800

    Add modal form and smooth scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,37 @@
-if (module.hot) {
-	module.hot.accept();
-}
-console.log('Hello World');
+'use strict';
+
+// if (module.hot) {
+// 	module.hot.accept();
+// }
+// console.log('Hello World');
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+
+const openModal = function (e) {
+	e.preventDefault();
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+	e.preventDefault();
+
+	// Matching strategy -- ignore clicks
+	if (e.target.classList.contains('nav__link')) {
+		const id = e.target.getAttribute('href');
+		document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+	}
+});
